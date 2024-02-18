c535bddc0346cdafdc4d32200ec5f558ab510875
tatianadeutsch <106558186+tatianadeutsch@users.noreply.github.com>
2024-02-18 06:19:30 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,12 +7,12 @@ from typing import Optional, Annotated
 from database import Base
 
 from sqlalchemy import Column, Integer, String, DateTime, Text
-# date = sqlalchemy.Column(sqlalchemy.date)
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
 
+
 class Clients(Base):
-    __tablename__ = 'clients'
+    __tablename__ = "clients"
 
     id_client: Mapped[intpk]
     name_client: Mapped[str] = mapped_column(String(180), nullable=False, unique=False)
@@ -21,7 +21,7 @@ class Clients(Base):
 
 
 class Employees(Base):
-    __tablename__ = 'employees'
+    __tablename__ = "employees"
 
     id_employee: Mapped[intpk]
     surname_employee: Mapped[str] = mapped_column(String(50), nullable=False)
@@ -34,10 +34,10 @@ class Employees(Base):
 
 
 class Products(Base):
-    __tablename__ = 'products'
+    __tablename__ = "products"
 
     id_product = Column(Integer(), primary_key=True)
-    id_procur = Column(Integer(), ForeignKey('procurement.id_procurement'))
+    id_procur = Column(Integer(), ForeignKey("procurement.id_procurement"))
     name_product = Column(String(100), nullable=False)
     specifications = Column(Text)
     description = Column(Text, nullable=False)
@@ -47,26 +47,26 @@ class Products(Base):
     quantity = Column(Integer, nullable=False)
     price_sale = Column(Numeric, nullable=False)
 
-    procurements_ = relationship('Procurement')
+    procurements_ = relationship("Procurement")
 
 
 class Orders(Base):
-    __tablename__ = 'orders'
+    __tablename__ = "orders"
 
     id_order: Mapped[intpk]
-    id_empl: Mapped[int] = mapped_column(ForeignKey('employees.id_employee'))
-    id_prod: Mapped[int] =  mapped_column(ForeignKey('products.id_product'))
+    id_empl: Mapped[int] = mapped_column(ForeignKey("employees.id_employee"))
+    id_prod: Mapped[int] = mapped_column(ForeignKey("products.id_product"))
     date_placement: Mapped[datetime.date]
     date_ex: Mapped[datetime.date]
-    id_cl: Mapped[int] =  mapped_column(ForeignKey('clients.id_client'))
+    id_cl: Mapped[int] = mapped_column(ForeignKey("clients.id_client"))
 
-    products_=relationship('Products')
-    employee_=relationship('Employees')
-    clients_=relationship('Clients')
+    products_ = relationship("Products")
+    employee_ = relationship("Employees")
+    clients_ = relationship("Clients")
 
 
 class Provider(Base):
-    __tablename__ = 'provider'
+    __tablename__ = "provider"
 
     id_provider: Mapped[intpk]
     name_provider: Mapped[str] = mapped_column(String(180), nullable=False)
@@ -77,30 +77,10 @@ class Provider(Base):
 
 
 class Procurement(Base):
-    __tablename__ = 'procurement'
+    __tablename__ = "procurement"
 
     id_procurement: Mapped[intpk]
-    id_prov: Mapped[int] = mapped_column(ForeignKey('provider.id_provider'))
+    id_prov: Mapped[int] = mapped_column(ForeignKey("provider.id_provider"))
     date_procur: Mapped[datetime.date]
 
-    provider_ = relationship('Provider')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    provider_ = relationship("Provider")
