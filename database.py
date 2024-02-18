289d20046a82f07c480c5cea008d06d5543447c7
tatianadeutsch <106558186+tatianadeutsch@users.noreply.github.com>
2024-02-18 06:20:01 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,11 +7,20 @@ engine = create_engine(conf_url, echo=True)
 
 
 # Создание сессии
-# Session = sessionmaker(bind=engine)
 session_ = sessionmaker(bind=engine)
+
 
 # Объявление базового класса
 class Base(DeclarativeBase):
     pass
 
-
+    # repr_cols_num = 5
+    # repr_cols = tuple()
+    #
+    # def __repr__(self):
+    #     cols = []
+    #     for idx, col in enumerate(self.__table__.columns.keys()):
+    #         if col in self.repr_cols or idx < self.repr_cols_num:
+    #             cols.append(f'{col}={getattr(self, col)}')
+    #
+    #     return f'<{self.__class__.__name__} {", ".join(cols)}>'
