83b8954e7e4b0fe8a90f2b467235f51c25cbafb1
tatianadeutsch <106558186+tatianadeutsch@users.noreply.github.com>
2024-02-18 06:20:42 +0300

    Update and rename main_2.py to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from orm_2 import (
+    create_table,
+    insert_table,
+    select_table,
+    update_table,
+    select_from_products,
+    select_from_products_1,
+    update_table_1,
+    select_with_join,
+    select_with_allias,
+    diff_date,
+    select_with_agr,
+    select_with_distinct,
+)
+
+# create_table()
+# insert_table()
+# select_table()
+# update_table()
+# select_from_products()
+# update_table_1()
+# select_from_products_1()
+# select_with_join()
+# select_with_allias()
+# diff_date()
+# select_with_agr()
+select_with_distinct()
